9329ac17ef762313f160062601ce53c33df0d5c5
kirkwoodwest <amtrax@gmail.com>
2022-11-28 14:15:06 -0800

    Plugin Window Control Version Update

--- src/main/java/com/kirkwoodwest/PluginWindowControlExtensionDefinition.java
+++ src/main/java/com/kirkwoodwest/PluginWindowControlExtensionDefinition.java
@@ -29,7 +29,7 @@ public class PluginWindowControlExtensionDefinition extends ControllerExtensionD
    @Override
    public String getVersion()
    {
-      return "0.2";
+      return "0.3";
    }
 
    @Override
