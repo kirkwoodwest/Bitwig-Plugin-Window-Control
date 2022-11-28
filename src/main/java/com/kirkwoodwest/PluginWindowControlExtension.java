cedf1260ff772228adf57b69fd11ee1ac4d8d774
kirkwoodwest <amtrax@gmail.com>
2022-11-28 13:20:36 -0800

    Added Open All Devices

--- src/main/java/com/kirkwoodwest/PluginWindowControlExtension.java
+++ src/main/java/com/kirkwoodwest/PluginWindowControlExtension.java
@@ -10,7 +10,10 @@ public class PluginWindowControlExtension extends ControllerExtension {
 
   private Signal settingOpenWindows;
   private Signal settingCloseWindows;
-  private ArrayList<Device> deviceList = new ArrayList<>();
+  private ArrayList<Device> cursorTrackDeviceList = new ArrayList<>();
+  private ArrayList<Device> bankDeviceList = new ArrayList<>();
+  private Signal settingAllOpenWindows;
+  private Signal settingAllCloseWindows;
 
 
   protected PluginWindowControlExtension(final PluginWindowControlExtensionDefinition definition, final ControllerHost host) {
@@ -28,58 +31,55 @@ public class PluginWindowControlExtension extends ControllerExtension {
     // For now just show a popup notification for verification that it is running.
     host.showPopupNotification("Plugin Window Control Initialized");
 
-    CursorTrack cursorTrack = host.createCursorTrack("Cursor Track", "Cursor Track", 0, 0, true);
-    DeviceBank  deviceBank  = cursorTrack.createDeviceBank(NUM_DEVICES);
-    for (int i = 0; i < NUM_DEVICES; i++) {
-      Device device = deviceBank.getDevice(i);
-      processDevice(device);
+    int NUM_TRACKS = 32;
+    TrackBank trackBank = host.createTrackBank(NUM_TRACKS, 0, 0, true);
 
-       DeviceBank db = device.createSiblingsDeviceBank(NUM_DEVICES);
-      for (int j = 0; j < NUM_DEVICES; j++) {
-       Device d2 = db.getItemAt(j);
-       processDevice(d2);
-      }
+    for (int i = 0; i < NUM_TRACKS; i++) {
+      Channel channel = trackBank.getItemAt(i);
+      processChannel(channel, bankDeviceList);
     }
+    MasterTrack masterTrack = host.createMasterTrack(0);
+    processChannel(masterTrack, bankDeviceList);
+    
+    CursorTrack cursorTrack = host.createCursorTrack("Cursor Track", "Cursor Track", 0, 0, true);
+    processChannel(cursorTrack, cursorTrackDeviceList);
 
+    settingOpenWindows = host.getDocumentState().getSignalSetting("Open", "Plugin Window Management", "Open Plugins");
+    settingCloseWindows = host.getDocumentState().getSignalSetting("Close", "Plugin Window Management", "Close Plugins");
+    settingOpenWindows.addSignalObserver(() -> showPluginWindows(true, cursorTrackDeviceList));
+    settingCloseWindows.addSignalObserver(() -> showPluginWindows(false, cursorTrackDeviceList));
 
-    settingOpenWindows = host.getDocumentState().getSignalSetting("Open", "Window Management", "Open Plugin Windows");
-    settingCloseWindows = host.getDocumentState().getSignalSetting("Close", "Window Management", "Close Plugin Windows");
-    settingOpenWindows.addSignalObserver(() -> showPluginWindows(true));
-    settingCloseWindows.addSignalObserver(() -> showPluginWindows(false));
+    settingAllOpenWindows = host.getDocumentState().getSignalSetting("Open", "All Plugin Window Management", "Open All Plugins ");
+    settingAllCloseWindows = host.getDocumentState().getSignalSetting("Close", "All Plugin Window Management", "Close All Plugins");
+    settingAllOpenWindows.addSignalObserver(() -> showPluginWindows(true, bankDeviceList));
+    settingAllCloseWindows.addSignalObserver(() -> showPluginWindows(false, bankDeviceList));
   }
 
-   private void processDevice(Device  device ) {
-     addDevice(device);
+  private void processChannel(Channel channel, ArrayList<Device> deviceList){
+    DeviceBank  deviceBank  = channel.createDeviceBank(NUM_DEVICES);
+    for (int i = 0; i < NUM_DEVICES; i++) {
+      Device device = deviceBank.getDevice(i);
+      processDevice(device, deviceList);
+      
+    }
+
+  }
+
+   private void processDevice(Device  device, ArrayList<Device> deviceList ) {
+     addDevice(device, deviceList);
      DeviceLayerBank layerBank = device.createLayerBank(NUM_LAYERS);
      for (int j = 0; j < NUM_LAYERS; j++) {
         DeviceBank layerDeviceBank = layerBank.getItemAt(j).createDeviceBank(NUM_DEVICES);
 
         for (int k = 0; k < NUM_DEVICES; k++) {
            Device device2 = layerDeviceBank.getItemAt(k);
-           addDevice(device2);
+           addDevice(device2, deviceList);
         }
      }
-
-//     DeviceBank deviceChain = device.deviceChain().createDeviceBank(NUM_DEVICES);
-//
-//     for (int a = 0; a < NUM_LAYERS; a++) {
-//        Device device2 = deviceChain.getItemAt(a);
-//        addDevice(device2);
-//
-//        DeviceLayerBank layerBank2 = device2.createLayerBank(NUM_LAYERS);
-//        for (int d = 0; d < NUM_LAYERS; d++) {
-//           DeviceBank layerDeviceBank = layerBank2.getItemAt(d).createDeviceBank(NUM_DEVICES);
-//           for (int k = 0; k < NUM_DEVICES; k++) {
-//              Device device3 = layerDeviceBank.getDevice(k);
-//              addDevice(device3);
-//           }
-//        }
-//     }
-
   }
 
-  private void showPluginWindows(boolean b) {
-    for (Device device : deviceList) {
+  private void showPluginWindows(boolean b, ArrayList<Device> devices) {
+    for (Device device : devices) {
       if (device.isPlugin().get()) {
         if(device.isWindowOpen().get() != b) {
           device.isWindowOpen().set(b);
@@ -90,7 +90,7 @@ public class PluginWindowControlExtension extends ControllerExtension {
   }
 
 
-  private void addDevice(Device device) {
+  private void addDevice(Device device, ArrayList<Device> deviceList) {
     device.isPlugin().markInterested();
     device.isWindowOpen().markInterested();
     device.name().markInterested();
